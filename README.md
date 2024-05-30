3efb3e00a908503c650eace1316763eaaddb31de
ahran_.choi <carokq@gmail.com>
2024-05-31 08:42:16 +0900

    default Settings

---
+++ README.md
@@ -0,0 +1,3 @@
+### Similarity Check
+---
+SimilarityCheck TDD
---
+++ Sample-Test1/SimilarityCheckerTest.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+#include "../SimilarityCheck/SimilarityChecker.cpp"
+
+TEST(SimilarityCheckerTest, TC0) {
+  EXPECT_EQ(1, 1);
+}
